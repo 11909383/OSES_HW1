e3cbade6c1d8f34af1ae181a770cb6b5952c5ad2
Oscar Steiner <scrsteiner@gmail.com>
2025-03-19 14:01:29 +0100

    Add tests

---
+++ test_playing_card_game.py
@@ -0,0 +1,11 @@
+from playing_card_game import deal_cards
+from playing_card_game import calculate_points
+
+def test_deal_cards():
+    #Test that exactly 5 cards have been drawn
+    hand = deal_cards()
+    assert len(hand) == 5
+
+def test_calculate_points():
+    hand = ['2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Spades', '6 of Clubs']
+    assert calculate_points(hand) == 9
