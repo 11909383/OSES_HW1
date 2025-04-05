1a21ddda0e6178d6c82bd8d7a7ecc9be8b2d3b89
11909383 <163847994+11909383@users.noreply.github.com>
2025-04-06 00:48:11 +0200

    Update test_playing_card_game.py
    changed two numbers to fail the pytest

--- test_playing_card_game.py
+++ test_playing_card_game.py
@@ -4,8 +4,8 @@ from playing_card_game import calculate_points
 def test_deal_cards():
     #Test that exactly 5 cards have been drawn
     hand = deal_cards()
-    assert len(hand) == 5
+    assert len(hand) == 6
 
 def test_calculate_points():
     hand = ['2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Spades', '6 of Clubs']
-    assert calculate_points(hand) == 9
+    assert calculate_points(hand) == 12
